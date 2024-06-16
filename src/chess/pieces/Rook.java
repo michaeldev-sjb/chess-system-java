e02c6d4f021e50d0e28f92dd4b31b005a3fb1d85
Michael Goncalves <michaeldev.sjb@gmail.com>
2024-06-16 10:30:33 -0300

    Implementing possibel moves of King

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -42,8 +42,8 @@ public class Rook extends ChessPiece {
             mat[p.getRow()][p.getColumn()] = true;
         }
 
-        //
-        //left
+
+        //right
         p.setValues(position.getRow(), position.getColumn() +1);
         while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
             mat[p.getRow()][p.getColumn()] = true;
