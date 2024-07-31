89b50fa72e9e4d0600bbe01c3454c1c576a03538
Michael Goncalves <michaeldev.sjb@gmail.com>
2024-07-31 06:55:02 -0300

    Check logic

--- src/Main.java
+++ src/Main.java
@@ -33,7 +33,7 @@ public class Main {
 
                 ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 
-                if (capturedPiece != null){
+                if (capturedPiece != null) {
                     captured.add(capturedPiece);
                 }
 
